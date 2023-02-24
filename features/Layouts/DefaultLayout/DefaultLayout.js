95aef95958c9ab349a25005e10ad86de71939226
ihor-vip <vip20140902@gmail.com>
2023-02-24 10:51:33 +0200

    create layout component for app

---
+++ features/Layouts/DefaultLayout/DefaultLayout.js
@@ -0,0 +1,13 @@
+import Footer from "@/features/common/modules/Footer";
+import Navigation from "@/features/common/modules/Navigation";
+
+const DefaultLayout = () => {
+    return (
+        <>
+            <Navigation/>
+            <Footer/>
+        </>
+    )
+}
+
+export default DefaultLayout;
